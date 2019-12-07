9c602ca237648461bbca6b860d1c99e0bb5fb74d
David Paschich <dpassage@balveda.com>
2019-12-06 17:56:02 -0800

    WIP day 4 part 2

--- Advent2019.playground/Pages/Day 4 - Secure Container.xcplaygroundpage/Contents.swift
+++ Advent2019.playground/Pages/Day 4 - Secure Container.xcplaygroundpage/Contents.swift
@@ -8,6 +8,16 @@ print(isValid("111111"))
 print(isValid("223450"))
 print(isValid("123789"))
 
+
+print("part2")
+print(part2valid("111111"))
+print(part2valid("223450"))
+print(part2valid("123789"))
+
+print(part2valid("112233"))
+print(part2valid("123444"))
+print(part2valid("111122"))
+
 var validCount = 0
 for password in 353096...843212 {
     if isValid(String(password)) {
@@ -15,5 +25,15 @@ for password in 353096...843212 {
     }
 }
 print(validCount)
+
+var part2count = 0
+for password in 353096...843212 {
+    if part2valid(String(password)) {
+        part2count += 1
+    }
+}
+
+// 327 is too low
+print(part2count)
 
 //: [Next](@next)
--- Advent2019.playground/Pages/Day 4 - Secure Container.xcplaygroundpage/Sources/Valid.swift
+++ Advent2019.playground/Pages/Day 4 - Secure Container.xcplaygroundpage/Sources/Valid.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public func isValid(_ password: String) -> Bool {
-    var points = [UInt8](password.utf8)
+    let points = [UInt8](password.utf8)
     var hasDouble = false
     for i in 0..<(points.count - 1) {
         if points[i] > points[i + 1] {
@@ -11,5 +11,26 @@ public func isValid(_ password: String) -> Bool {
             hasDouble = true
         }
     }
+    return hasDouble
+}
+
+public func part2valid(_ password: String) -> Bool {
+    let points = [UInt8](password.utf8)
+    var hasDouble = false
+    var currentRunLength = 0
+    for i in 0..<(points.count - 1) {
+        if points[i] > points[i + 1] {
+            return false
+        }
+        if points[i] == points[i + 1] {
+            currentRunLength += 1
+        } else {
+            if currentRunLength == 2 {
+                hasDouble = true
+            }
+            currentRunLength = 1
+        }
+    }
+    if currentRunLength == 2 { return true }
     return hasDouble
 }
